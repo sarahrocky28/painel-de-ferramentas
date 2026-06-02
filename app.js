3c3dbd00f69c7b047dd66afede9509bfaa88a483
sarahrocky28 <sarah109923753@gmail.com>
2026-06-01 21:05:06 -0300

    desenvolvendo função para js

--- app.js
+++ app.js
@@ -1,5 +1,24 @@
 const elemento = {
-    aside: documento.querySelector("aside")
+    aside: documento.querySelector("aside"),
+    section: document.querySelectorAll("section"),
 }
 
-console.log(elemento.aside)
+console.log(elemento.aside);
+
+elemento.aside.addEventListener("click", (evento) => {
+    evento.preventDefault();
+    console.log(evento.target.id);
+    displayController(evento.target.id)
+});
+
+function displayController(id) {
+    elemento.sections.array.forEach((section) => {
+        if (!section.classList.contains("hidden")) {
+            section.classList.add("hidden");
+        }
+
+        //Remove a classe "hidden" do item clicado
+        if (section.classList.contains(id))
+            section.classList.remove
+    });
+}
